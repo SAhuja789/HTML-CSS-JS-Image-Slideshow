8f28b654676b96d5c7dd9223bd3f3618789e60c6
SAhuja789 <70078744+SAhuja789@users.noreply.github.com>
2020-10-22 14:56:25 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slide.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+console.log("Link for Tutorial: https://www.w3schools.com/howto/howto_js_slideshow.asp");
+
+// You may edit and add to this script if you are to use it as your main script file
